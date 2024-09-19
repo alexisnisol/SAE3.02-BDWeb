ff64d428c4ce2c3d864ff60e1d471ae910e496d4
Wiciak Alexy <o22300674@or-iut-i202-f14.iut45.univ-orleans.fr>
2024-09-19 14:08:11 +0200

    ajout de la BD avec la suppression

---
+++ SAE_WEB_BD.sql
@@ -0,0 +1,77 @@
+--CREATION DE LA BASE DE DONNEES--
+
+
+CREATE TABLE COURS_PROGRAMME (
+  id_cours INT NOT NULL AUTO_INCREMENT,
+  nom_cours VARCHAR(42),
+  niveau INT,
+  duree INT,
+  heure TIME,
+  jour ENUM('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'),
+  Ddd DATE,
+  Ddf DATE,
+  nb_personnes_max INT,
+  PRIMARY KEY (id_cours)
+);
+
+CREATE TABLE COURS_REALISE (
+  id_cours INT NOT NULL,
+  date DATE NOT NULL,
+  id_personne VARCHAR(42) NOT NULL,
+  PRIMARY KEY (id_cours, date),
+  FOREIGN KEY (id_cours) REFERENCES COURS_PROGRAMME (id_cours),
+  FOREIGN KEY (id_personne) REFERENCES PERSONNE (id_personne)
+);
+
+CREATE TABLE PERSONNE (
+  id_personne VARCHAR(42) NOT NULL,
+  nom VARCHAR(42),
+  prenom VARCHAR(42),
+  adresse VARCHAR(100),
+  telephone VARCHAR(15),
+  email VARCHAR(100),
+  experience TEXT NULL,
+  salaire DECIMAL(10, 2) NULL,
+  poids FLOAT NULL,
+  cotisation DECIMAL(10, 2) NULL,
+  date_inscription DATE NULL,
+  niveau INT NULL,
+  PRIMARY KEY (id_personne)
+);
+
+CREATE TABLE PONEY (
+  id_poney VARCHAR(42) NOT NULL,
+  nom VARCHAR(42),
+  age INT,
+  poids_max FLOAT,
+  heures_travail INT,
+  PRIMARY KEY (id_poney)
+);
+
+CREATE TABLE RESERVER (
+  id_personne_composee VARCHAR(42) NOT NULL,
+  id_poney_composante VARCHAR(42) NOT NULL,
+  id_cours INT NOT NULL,
+  date DATE NOT NULL,
+  PRIMARY KEY (id_personne_composee, id_poney_composante, id_cours, date)
+);
+
+ALTER TABLE COURS_REALISE
+  ADD CONSTRAINT fk_cours
+  FOREIGN KEY (id_cours) REFERENCES COURS_PROGRAMME (id_cours);
+
+ALTER TABLE COURS_REALISE
+  ADD CONSTRAINT fk_personne
+  FOREIGN KEY (id_personne) REFERENCES PERSONNE (id_personne);
+
+ALTER TABLE RESERVER
+  ADD CONSTRAINT fk_personne_composee
+  FOREIGN KEY (id_personne_composee) REFERENCES PERSONNE (id_personne);
+
+ALTER TABLE RESERVER
+  ADD CONSTRAINT fk_poney_composante
+  FOREIGN KEY (id_poney_composante) REFERENCES PONEY (id_poney);
+
+ALTER TABLE RESERVER
+  ADD CONSTRAINT fk_cours_date
+  FOREIGN KEY (id_cours, date) REFERENCES COURS_REALISE (id_cours, date);
