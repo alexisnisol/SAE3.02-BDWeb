d0871c73df22e652851bdd15d4a9aa1e555e505a
Wiciak Alexy <o22300674@or-iut-i202-f14.iut45.univ-orleans.fr>
2024-09-19 14:13:55 +0200

    correction bd

--- SAE_WEB_BD.sql
+++ SAE_WEB_BD.sql
@@ -1,6 +1,18 @@
 --CREATION DE LA BASE DE DONNEES--
+DROP TABLE IF EXISTS RESERVER;
+DROP TABLE IF EXISTS COURS_REALISE;
+DROP TABLE IF EXISTS PONEY;
+DROP TABLE IF EXISTS PERSONNE;
+DROP TABLE IF EXISTS COURS_PROGRAMME;
 
+-- Suppression des tables si elles existent déjà
+DROP TABLE IF EXISTS RESERVER;
+DROP TABLE IF EXISTS COURS_REALISE;
+DROP TABLE IF EXISTS PONEY;
+DROP TABLE IF EXISTS PERSONNE;
+DROP TABLE IF EXISTS COURS_PROGRAMME;
 
+-- Création de la table COURS_PROGRAMME
 CREATE TABLE COURS_PROGRAMME (
   id_cours INT NOT NULL AUTO_INCREMENT,
   nom_cours VARCHAR(42),
@@ -14,15 +26,7 @@ CREATE TABLE COURS_PROGRAMME (
   PRIMARY KEY (id_cours)
 );
 
-CREATE TABLE COURS_REALISE (
-  id_cours INT NOT NULL,
-  date DATE NOT NULL,
-  id_personne VARCHAR(42) NOT NULL,
-  PRIMARY KEY (id_cours, date),
-  FOREIGN KEY (id_cours) REFERENCES COURS_PROGRAMME (id_cours),
-  FOREIGN KEY (id_personne) REFERENCES PERSONNE (id_personne)
-);
-
+-- Création de la table PERSONNE
 CREATE TABLE PERSONNE (
   id_personne VARCHAR(42) NOT NULL,
   nom VARCHAR(42),
@@ -39,6 +43,7 @@ CREATE TABLE PERSONNE (
   PRIMARY KEY (id_personne)
 );
 
+-- Création de la table PONEY
 CREATE TABLE PONEY (
   id_poney VARCHAR(42) NOT NULL,
   nom VARCHAR(42),
@@ -48,30 +53,24 @@ CREATE TABLE PONEY (
   PRIMARY KEY (id_poney)
 );
 
+-- Création de la table COURS_REALISE avec clés étrangères
+CREATE TABLE COURS_REALISE (
+  id_cours INT NOT NULL,
+  date DATE NOT NULL,
+  id_personne VARCHAR(42) NOT NULL,
+  PRIMARY KEY (id_cours, date),
+  FOREIGN KEY (id_cours) REFERENCES COURS_PROGRAMME (id_cours),
+  FOREIGN KEY (id_personne) REFERENCES PERSONNE (id_personne)
+);
+
+-- Création de la table RESERVER avec clés étrangères
 CREATE TABLE RESERVER (
   id_personne_composee VARCHAR(42) NOT NULL,
   id_poney_composante VARCHAR(42) NOT NULL,
   id_cours INT NOT NULL,
   date DATE NOT NULL,
-  PRIMARY KEY (id_personne_composee, id_poney_composante, id_cours, date)
+  PRIMARY KEY (id_personne_composee, id_poney_composante, id_cours, date),
+  FOREIGN KEY (id_personne_composee) REFERENCES PERSONNE (id_personne),
+  FOREIGN KEY (id_poney_composante) REFERENCES PONEY (id_poney),
+  FOREIGN KEY (id_cours, date) REFERENCES COURS_REALISE (id_cours, date)
 );
-
-ALTER TABLE COURS_REALISE
-  ADD CONSTRAINT fk_cours
-  FOREIGN KEY (id_cours) REFERENCES COURS_PROGRAMME (id_cours);
-
-ALTER TABLE COURS_REALISE
-  ADD CONSTRAINT fk_personne
-  FOREIGN KEY (id_personne) REFERENCES PERSONNE (id_personne);
-
-ALTER TABLE RESERVER
-  ADD CONSTRAINT fk_personne_composee
-  FOREIGN KEY (id_personne_composee) REFERENCES PERSONNE (id_personne);
-
-ALTER TABLE RESERVER
-  ADD CONSTRAINT fk_poney_composante
-  FOREIGN KEY (id_poney_composante) REFERENCES PONEY (id_poney);
-
-ALTER TABLE RESERVER
-  ADD CONSTRAINT fk_cours_date
-  FOREIGN KEY (id_cours, date) REFERENCES COURS_REALISE (id_cours, date);
