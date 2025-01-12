3b2e10582b0078313754382cf22c39afd112dbba
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2025-01-12 17:48:37 +0100

    Correction script bd

--- src/static/data/insPon.sql
+++ src/static/data/insPon.sql
@@ -5,6 +5,8 @@ DELETE FROM COURS_PROGRAMME;
 DELETE FROM PERSONNE;
 DELETE FROM PONEY;
 
+ALTER TABLE COURS_PROGRAMME AUTO_INCREMENT = 1;
+
 -- Insertion dans PERSONNE
 
 -- Poids limite mais valide : Client
@@ -23,34 +25,34 @@ VALUES ('Durand', 'Claire', '789 Rue de Marseille', '0102030406', 'claire@exampl
 
 -- Valide
 INSERT INTO PONEY (nom, age, poids_max)
-VALUES 
-  ('Spirit', 10, 45.0);
+VALUES
+    ('Spirit', 10, 45.0);
 
 -- Valide
 INSERT INTO PONEY (nom, age, poids_max)
-VALUES 
-  ('Black Beauty', 12, 40.0);
+VALUES
+    ('Black Beauty', 12, 40.0);
 
 -- Test inserting valid data
 INSERT INTO COURS_PROGRAMME (nom_cours, niveau, duree, heure, jour, Ddd, Ddf, nb_personnes_max)
-VALUES 
-  ('Cours Debutant', 1, 2, '10:00:00', 'Lundi', '2024-01-01', '2024-01-07', 2),
-  ('Cours Avance', 2, 2, '14:00:00', 'Mardi', '2024-01-08', '2024-01-14', 3),
-  ('Cours Expert', 5, 1, '09:00:00', 'Mercredi', '2024-01-15', '2024-01-21', 5),
-  ('Cours de saut', 3, 1, '10:00:00', 'Lundi', '2024-11-21', '2024-12-21', 5),
-  ('Cours Apprentissage', 1, 2, '10:00:00', 'Lundi', '2024-01-01', '2024-01-10', 5),
-  ('Cours Apprentissage', 1, 2, '12:00:00', 'Lundi', '2024-01-01', '2024-01-10', 5),
-  ('Cours Apprentissage', 1, 2, '13:00:00', 'Lundi', '2024-01-01', '2024-01-10', 5);
+VALUES
+    ('Cours Debutant', 1, 2, '10:00:00', 'Lundi', '2024-01-01', '2024-01-07', 2),
+    ('Cours Avance', 2, 2, '14:00:00', 'Mardi', '2024-01-08', '2024-01-14', 3),
+    ('Cours Expert', 3, 1, '09:00:00', 'Mercredi', '2024-01-15', '2024-01-21', 5),
+    ('Cours de saut', 3, 1, '10:00:00', 'Lundi', '2024-11-21', '2024-12-21', 5),
+    ('Cours Apprentissage', 1, 2, '10:00:00', 'Lundi', '2024-01-01', '2024-01-10', 5),
+    ('Cours Apprentissage', 1, 2, '12:00:00', 'Lundi', '2024-01-01', '2024-01-10', 5),
+    ('Cours Apprentissage', 1, 2, '13:00:00', 'Lundi', '2024-01-01', '2024-01-10', 5);
 
 -- Test inserting valid data
 INSERT INTO COURS_REALISE (id_cours, id_moniteur, dateR)
-VALUES 
-  (1, 3, '2024-01-01 10:00:00'),
-  (2, 3, '2024-01-09 14:00:00'),
-  (3, 3, '2024-01-21 09:00:00'),
-  (5, 3, '2024-01-09 10:00:00'),
-  (6, 3, '2024-01-09 12:00:00'),
-  (7, 3, '2024-01-09 13:00:00');
+VALUES
+    (1, 3, '2024-01-01 10:00:00'),
+    (2, 3, '2024-01-09 14:00:00'),
+    (3, 3, '2024-01-21 09:00:00'),
+    (5, 3, '2024-01-09 10:00:00'),
+    (6, 3, '2024-01-09 12:00:00'),
+    (7, 3, '2024-01-09 13:00:00');
 
 
 -- Cas valide
