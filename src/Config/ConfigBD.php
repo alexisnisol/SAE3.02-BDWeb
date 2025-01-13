1db8b14cb2311123d87ec041b9272a25c4d6d815
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2025-01-13 11:32:12 +0100

    Fix bd

--- src/Config/ConfigBD.php
+++ src/Config/ConfigBD.php
@@ -5,9 +5,9 @@ namespace Config;
 
 class ConfigBD {
     public static $DB_HOST = 'servinfo-maria';
-    public static $DB_NAME = 'DBroot';
-    public static $DB_USER = 'root';
-    public static $DB_PASSWORD = 'root';
+    public static $DB_NAME = 'DBnisol';
+    public static $DB_USER = 'nisol';
+    public static $DB_PASSWORD = 'nisol';
 }
 
 ?>
