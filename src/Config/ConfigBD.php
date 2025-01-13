c0ce2d27ba9b06879c1b02fc95fe467092ededc0
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2025-01-13 11:30:35 +0100

    Ajout ConfigBD par défaut

---
+++ src/Config/ConfigBD.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Config;
+
+
+class ConfigBD {
+    public static $DB_HOST = 'servinfo-maria';
+    public static $DB_NAME = 'DBroot';
+    public static $DB_USER = 'root';
+    public static $DB_PASSWORD = 'root';
+}
+
+?>
